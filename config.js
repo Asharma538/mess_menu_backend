67f959bb9286add495e15eed62e7270f54a2f8e3
Anadi Sharma <s.anadi0503+1@gmail.com>
2025-07-12 21:29:00 +0530

    update with sheets api added

--- config.js
+++ config.js
@@ -3,15 +3,7 @@ const { initializeAuth , signInWithEmailAndPassword , createUserWithEmailAndPass
 const { getFirestore, collection } = require('firebase/firestore');
 const { email , password } = require('./secret_files/signin_params.json');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAoCygpXSFmbCHJGwQVgGJMwqkXLXTvIXE",
-  authDomain: "iitj-menu-2507.firebaseapp.com",
-  projectId: "iitj-menu-2507",
-  storageBucket: "iitj-menu-2507.appspot.com",
-  messagingSenderId: "691003196896",
-  appId: "1:691003196896:web:738dd783745be2caf91785",
-  measurementId: "G-5TPRW5W4CW",
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || `{}`);
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app);
